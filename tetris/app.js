980ac7bd9c0bf08f615a997a13b56a31a76643c9
whoana <whoana@gmail.com>
2022-01-04 22:12:35 +0900

    from home2

--- tetris/app.js
+++ tetris/app.js
@@ -13,6 +13,41 @@ document.addEventListener('DOMContentLoaded',() => {
 
     }
 
-    console.log(playground);
+    const iTetromino = [
+        [
+            [0,0],
+            [1,0],
+            [2,0],
+            [3,0]
+        ]
+    ]
+    const tetrominos = [iTetromino]
+
+    let deltaX = 4;
+    let deltaY = 0;
+    tetrominos[0][0].forEach(element => {
+        let row = element[0] + deltaY
+        let col = element[1] + deltaX
+        playground[row][col].classList.add('tetromino')
+        //playground[element[0]][element[1]].classList.remove('tetromino')
+    })
+
+    let timeId = setInterval(()=>{
+
+        tetrominos[0][0].forEach(element => {
+            let row = element[0] + deltaY
+            let col = element[1] + deltaX
+            playground[row][col].classList.remove('tetromino')
+        })
+        
+        deltaY = deltaY + 1
+        
+        tetrominos[0][0].forEach(element => {
+            let row = element[0] + deltaY
+            let col = element[1] + deltaX
+            playground[row][col].classList.add('tetromino')
+        })
+
+    }, 1000)
 
 })
